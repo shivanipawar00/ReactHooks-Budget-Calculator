1de7f524b5af514d6427888ccd4a7aa46b7924d5
shivani <pawarshivani00@gmail.com>
2020-10-16 00:13:32 +0530

    initial commit

---
+++ budget-calculator/src/components/ExpenseItem.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import {MdEdit, MdDelete} from 'react-icons/md'
+export const ExpenseItem = ({expense,handleDelete,handleEdit}) => {
+    const {id,charge,amount} = expense;
+    return (
+        <li className="item">
+            <div className="info">
+                <span className="expanse">{charge}</span>
+                <span className="amount">${amount}</span>
+            </div>
+            <div>
+                <button className="edit-btn" aria-label="edit button" onClick={()=>handleEdit(id)}>
+                    <MdEdit/>
+                </button>
+                <button className="clear-btn" aria-label="delete button" onClick={()=>handleDelete(id)}>
+                    <MdDelete/>
+                </button>
+            </div>
+        </li>
+    )
+}
